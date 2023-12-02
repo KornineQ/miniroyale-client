1ede9abb2fa36a20d24e784dcc2797b86f61f317
KornineQ <84327578+KornineQ@users.noreply.github.com>
2023-12-02 16:03:00 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,7 @@
-// preload.js
 const { contextBridge, ipcRenderer, remote } = require('electron');
 
-// Expose custom API to the renderer process
 contextBridge.exposeInMainWorld('myCustomAPI', {
   blockSocialEmbeds: () => {
-    // Example: Block site embeds from social media
     const iframes = document.querySelectorAll('iframe');
     iframes.forEach((iframe) => {
       const src = iframe.getAttribute('src');
@@ -15,7 +12,6 @@ contextBridge.exposeInMainWorld('myCustomAPI', {
   },
 
   disableF11Fullscreen: () => {
-    // Example: Disable F11 key for fullscreen
     document.addEventListener('keydown', (event) => {
       if (event.key === 'F11') {
         event.preventDefault();
@@ -28,14 +24,11 @@ contextBridge.exposeInMainWorld('myCustomAPI', {
   },
 });
 
-// You can also add more functionality or access Node.js modules here
 
-// Example: Add a listener for IPC messages from the main process
 ipcRenderer.on('main-process-message', (event, message) => {
   console.log('Received message from main process:', message);
 });
 
-// Example: Use remote module to access main process functionality
 const { getCurrentWindow } = remote;
 const currentWindow = getCurrentWindow();
 console.log('Current window title:', currentWindow.getTitle());
